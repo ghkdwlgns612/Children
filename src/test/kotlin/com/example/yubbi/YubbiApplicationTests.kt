9b0aa9c79b262f19ea2c3593e40b12971eaeb12c
jihuhwan-kids <intern220495@github.com>
2022-04-18 20:48:42 +0900

    first commit

---
+++ src/test/kotlin/com/example/yubbi/YubbiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.yubbi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YubbiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
