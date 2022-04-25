bf89aae93d081bc1ade184cf9cbe267921408409
황지훈(LGU/인턴) <103920798+jihuhwan-kids@users.noreply.github.com>
2022-04-25 15:20:36 +0900

    [BABY1-45] feature : JPA의존성 관리 및 Mysql DB연동 (#5)
    * jpa + mysql 플러그인 및 의존성 생성
    
    * application.yml파일 작성.
    
    * 테스트용 application.yml파일 작성 및 h2 의존성 추가.
    
    Co-authored-by: jihuhwan-kids <intern220495@github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,12 @@ plugins {
     id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.5.21"
+    id("org.jetbrains.kotlin.plugin.noarg") version "1.5.21"
+
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
+    kotlin("plugin.jpa") version "1.5.21"
 }
 
 group = "com.example"
@@ -16,12 +20,25 @@ repositories {
     mavenCentral()
 }
 
+noArg { // “엔티티는 반드시 파라미터가 없는 (no-arg) public 또는 protected 생성자가 있어야 한다.” 자바 리플렉션 관련하여 넣음
+    annotation("javax.persistence.Entity")
+}
+
+allOpen { // 코틀린은 final이라 상속이 불가능. JPA Lazy Loading을 위해선 프록시 객체를 생성해야하므로 open class 생성해야함.
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.h2database:h2")
 }
 
 tasks.withType<KotlinCompile> {
