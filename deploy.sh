c97fda23089f701355497946247aa6d1a46fdc5c
황지훈(LGU/인턴) <103920798+jihuhwan-kids@users.noreply.github.com>
2022-05-08 13:43:09 +0900

    [BABY1-373] bug : deploy.sh수정 (#55)
    Co-authored-by: jihuhwan-kids <intern220495@github.com>

--- deploy.sh
+++ deploy.sh
@@ -18,10 +18,10 @@ if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다!"
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> sudo kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
 DEPLOY_JAR=/home/ec2-user/*.jar
-nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
+sudo nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
